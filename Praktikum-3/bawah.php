8dfd305ea7be08ed56982a4230ea7d37dd9ec857
raihanijuliani <raihanij588@gmail.com>
2022-03-27 06:54:26 +0700

    add Praktium-3

---
+++ Praktikum-3/bawah.php
@@ -0,0 +1,5 @@
+<div style="background-color: #1E90FF; height:60px;">
+<footer class="d-flex justify-content-center pt-3">
+<p>Created by <a class="text-dark" href="http://nurulfikri.ac.id">Pusinfo NF &copy;2017</a></p>
+</footer>
+</div>
